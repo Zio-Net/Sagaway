fb7db65ee61aa4df9e5ec79b4c7f3a268270becd
Alon Fliess <alonf@codevalue.net>
2024-03-01 03:59:42 +0200

    remove opneApi info

--- Sagaway.IntegrationTests/Sagaway.IntegrationTests.TestService/Program.cs
+++ Sagaway.IntegrationTests/Sagaway.IntegrationTests.TestService/Program.cs
@@ -38,115 +38,118 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapPost("/test-queue", async (
-        [FromBody] ServiceTestInfo request,
-        [FromHeader(Name = "x-sagaway-message-dispatch-time")] string messageDispatchTimeHeader,
-        [FromServices] ILogger<Program> logger,
-        [FromServices] ICallbackQueueNameProvider callbackQueueNameProvider,
-        [FromServices] DaprClient daprClient) =>
+    [FromBody] ServiceTestInfo request,
+    [FromHeader(Name = "x-sagaway-message-dispatch-time")]
+    string messageDispatchTimeHeader,
+    [FromServices] ILogger<Program> logger,
+    [FromServices] ICallbackQueueNameProvider callbackQueueNameProvider,
+    [FromServices] DaprClient daprClient) =>
+{
+    logger.LogInformation("Received test request: {request}", request);
+
+    var reservationId = request.CallId;
+
+    await Task.Delay(TimeSpan.FromSeconds(request.DelayOnCallInSeconds));
+
+    // Parse the dispatch time from the header
+    var decodedMessageDispatchTimeHeader = WebUtility.UrlDecode(messageDispatchTimeHeader);
+
+    if (DateTime.TryParseExact(decodedMessageDispatchTimeHeader, "o", CultureInfo.InvariantCulture,
+            DateTimeStyles.RoundtripKind, out var messageDispatchTime))
     {
-        logger.LogInformation("Received test request: {request}", request);
+        logger.LogInformation("Reservation: {reservationId}, Message dispatch time: {messageDispatchTime}",
+            reservationId, messageDispatchTime);
+    }
+    else
+    {
+        logger.LogError("Reservation: {reservationId}, Invalid message dispatch time format.",
+            reservationId);
+        return;
+    }
 
-        var reservationId = request.CallId;
-
-        await Task.Delay(TimeSpan.FromSeconds(request.DelayOnCallInSeconds));
-
-        // Parse the dispatch time from the header
-        var decodedMessageDispatchTimeHeader = WebUtility.UrlDecode(messageDispatchTimeHeader);
-
-        if (DateTime.TryParseExact(decodedMessageDispatchTimeHeader, "o", CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out var messageDispatchTime))
-        {
-            logger.LogInformation("Reservation: {reservationId}, Message dispatch time: {messageDispatchTime}",
-                reservationId, messageDispatchTime);
-        }
-        else
-        {
-            logger.LogError("Reservation: {reservationId}, Invalid message dispatch time format.",
-                reservationId);
-            return;
-        }
-
-        if (!request.ShouldSucceed)
-        {
-            logger.LogError("Test {request.CallId} did nothing and return a failure as requested",
-                request.CallId);
-
-            if (request.ShouldReturnCallbackResult)
-            {
-                logger.LogInformation("Sending callback failure result for test {request.CallId}",
-                                       request.CallId);
-                await daprClient.InvokeBindingAsync(callbackQueueNameProvider.CallbackQueueName, "create", false);
-            }
-
-            return;
-        }
-
-        // Set time to live for 2 seconds for delete, and 10 minutes to reset the test state for house cleaning
-        var metadata = new Dictionary<string, string>
-        {
-            { "ttlInSeconds", request.IsReverting ? "2" : "600" }
-        };
-
-       
-        var retryPolicy = Policy
-            .Handle<Exception>()
-            .RetryAsync(3, (exception, retryCount) =>
-            {
-                logger.LogWarning($"Retrying save state operation due to exception: {exception.Message}. Retry count: {retryCount}");
-            });
-
-        var result = true;
-        try
-        {
-            SaveState? savedState;
-            string? etag = null;
-
-            await retryPolicy.ExecuteAsync(async () =>
-            {
-                try
-                {
-                    (savedState, etag) =
-                        await daprClient.GetStateAndETagAsync<SaveState>("statestore", reservationId);
-
-                }
-                catch (DaprException ex) when (ex.HResult == 404) //not found
-                {
-                    
-                }
-
-                savedState = new()
-                {
-                    CallerId = request.CallId,
-                    MessageDispatchTime = messageDispatchTime
-                };
-
-                var saveResult = await daprClient.TrySaveStateAsync("statestore", request.CallId, savedState,
-                    etag, new StateOptions()
-                    {
-                        Consistency = ConsistencyMode.Strong
-                    }, metadata);
-
-                if (!saveResult)
-                {
-                    throw new Exception("Failed to save state");
-                }
-            });
-        }
-        catch (Exception e)
-        {
-            result = false;
-            logger.LogError(e, "Failed to do {action} for call id: {TestCallId} ",
-                request.IsReverting ? "revert" : "set", request.CallId);
-        }
+    if (!request.ShouldSucceed)
+    {
+        logger.LogError("Test {request.CallId} did nothing and return a failure as requested",
+            request.CallId);
 
         if (request.ShouldReturnCallbackResult)
         {
-            logger.LogInformation("Sending callback result for test {request.CallId}",
-                               request.CallId);
-            await daprClient.InvokeBindingAsync(callbackQueueNameProvider.CallbackQueueName, "create", result);
+            logger.LogInformation("Sending callback failure result for test {request.CallId}",
+                request.CallId);
+            await daprClient.InvokeBindingAsync(callbackQueueNameProvider.CallbackQueueName, "create", false);
         }
-    })
-    .WithName("CarReservationQueue")
-    .WithOpenApi();
+
+        return;
+    }
+
+    // Set time to live for 2 seconds for delete, and 10 minutes to reset the test state for house cleaning
+    var metadata = new Dictionary<string, string>
+    {
+        { "ttlInSeconds", request.IsReverting ? "2" : "600" }
+    };
+
+
+    var retryPolicy = Policy
+        .Handle<Exception>()
+        .RetryAsync(3, (exception, retryCount) =>
+        {
+            logger.LogWarning(
+                $"Retrying save state operation due to exception: {exception.Message}. Retry count: {retryCount}");
+        });
+
+    var result = true;
+    try
+    {
+        SaveState? savedState;
+        string? etag = null;
+
+        await retryPolicy.ExecuteAsync(async () =>
+        {
+            try
+            {
+                (savedState, etag) =
+                    await daprClient.GetStateAndETagAsync<SaveState>("statestore", reservationId);
+
+            }
+            catch (DaprException ex) when (ex.HResult == 404) //not found
+            {
+
+            }
+
+            savedState = new()
+            {
+                CallerId = request.CallId,
+                MessageDispatchTime = messageDispatchTime
+            };
+
+            var saveResult = await daprClient.TrySaveStateAsync("statestore", request.CallId, savedState,
+                etag, new StateOptions()
+                {
+                    Consistency = ConsistencyMode.Strong
+                }, metadata);
+
+            if (!saveResult)
+            {
+                throw new Exception("Failed to save state");
+            }
+        });
+    }
+    catch (Exception e)
+    {
+        result = false;
+        logger.LogError(e, "Failed to do {action} for call id: {TestCallId} ",
+            request.IsReverting ? "revert" : "set", request.CallId);
+    }
+
+    if (request.ShouldReturnCallbackResult)
+    {
+        logger.LogInformation("Sending callback result for test {request.CallId}",
+            request.CallId);
+        await daprClient.InvokeBindingAsync(callbackQueueNameProvider.CallbackQueueName, "create", result);
+    }
+}).ExcludeFromDescription();
+    
+    
 
 
 app.MapGet("/test/{callId}", async ([FromRoute] Guid callId, [FromServices] DaprClient daprClient, [FromServices] ILogger<Program> logger) =>
