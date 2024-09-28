b73b647777b840b142cc76ec2375b511d69152e6
Alon Fliess <alon@fliess.org>
2024-09-28 18:16:33 +0300

    Bug fix to ensure proper decoding of URL-encoded characters in Base64-decoded strings. (#66)
    * Add header propagation in SagawayContextPropagationHandler
    
    Enhanced SagawayContextPropagationHandler to conditionally add
    the following headers to the request if their values are present:
    - x-sagaway-dapr-callback-binding-name
    - x-sagaway-dapr-message-dispatch-time
    - x-sagaway-dapr-custom-metadata
    
    This ensures additional context information is propagated with
    the request for purposes such as tracing, logging, or custom
    processing on the receiving end.
    
    * Add URL decoding to Base64 to JSON conversion
    
    Included `System.Web` for `HttpUtility.UrlDecode` in `SagawayContextManager.cs`.
    Renamed `jsonString` to `jsonStringEncoded` in `ConvertFromBase64<T>`.
    Added URL decoding step to handle URL-encoded characters.
    
    ---------
    
    Co-authored-by: Alon Fliess <alonf@codevalue.net>

--- Sagaway.Callback.Propagator/SagawayContextManager.cs
+++ Sagaway.Callback.Propagator/SagawayContextManager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Text;
 using System.Text.Json;
+using System.Web;
 
 namespace Sagaway.Callback.Propagator;
 
@@ -147,7 +148,8 @@ public class SagawayContextManager : ISagawayContextManager
 
     public T ConvertFromBase64<T>(string base64String)
     {
-        var jsonString = Encoding.UTF8.GetString(Convert.FromBase64String(base64String));
+        var jsonStringEncoded = Encoding.UTF8.GetString(Convert.FromBase64String(base64String));
+        var jsonString = HttpUtility.UrlDecode(jsonStringEncoded);
         var context = JsonSerializer.Deserialize<T>(jsonString, _jsonSerializerOptions) ??
                       throw new InvalidOperationException("Can't deserialize base64String json content");
 
