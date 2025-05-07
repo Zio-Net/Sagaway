1893a959110242162d46e9336acdaebcbc9e4f80
yosefkahlon <yosefkahlon53@gmail.com>
2025-05-07 11:24:01 +0300

    Print the environment

--- Sagaway.ReservationDemo/Sagaway.ReservationDemo.ReservationUI/docker-entrypoint.sh
+++ Sagaway.ReservationDemo/Sagaway.ReservationDemo.ReservationUI/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Print the environment variable to confirm it's passed
+echo "RESERVATION_MANAGER_URL is: ${RESERVATION_MANAGER_URL}"
+
 # Replace environment variables in NGINX configuration
 envsubst '${RESERVATION_MANAGER_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
