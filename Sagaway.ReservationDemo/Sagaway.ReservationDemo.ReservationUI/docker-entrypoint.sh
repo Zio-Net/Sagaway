938710bc7b43007fcdcffbd471fd97ff5c8edbd0
yosefkahlon <yosefkahlon53@gmail.com>
2025-05-07 11:50:51 +0300

    Print the substituted Nginx config for debugging

--- Sagaway.ReservationDemo/Sagaway.ReservationDemo.ReservationUI/docker-entrypoint.sh
+++ Sagaway.ReservationDemo/Sagaway.ReservationDemo.ReservationUI/docker-entrypoint.sh
@@ -7,5 +7,10 @@ echo "RESERVATION_MANAGER_URL is: ${RESERVATION_MANAGER_URL}"
 # Replace environment variables in NGINX configuration
 envsubst '${RESERVATION_MANAGER_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
+# Print the substituted Nginx config for debugging
+echo "--- Substituted Nginx Config (/etc/nginx/conf.d/default.conf) ---"
+cat /etc/nginx/conf.d/default.conf
+echo "--- End of Substituted Nginx Config ---"
+
 # Execute the CMD from the Dockerfile
 exec "$@"
